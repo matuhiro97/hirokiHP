5a8fd8d9d42fca346eef25a72840f08ac402fb75
matuhiro97 <115542804+matuhiro97@users.noreply.github.com>
2024-10-04 01:23:04 +0900

    自己紹介pageに色付けた。

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
 const About: React.FC = () => {
   return (
-    <section id="about">
-      <h2>About</h2>
-      <p>ここに松本浩輝の自己紹介</p>
-    </section>
+    <Box
+    id="about"
+    sx={{
+      width: '100vw',            // 幅をビューポート全体に設定
+      height: '100vh',           // 高さをビューポート全体に設定
+      display: 'flex',           // Flexbox レイアウトを使用
+      alignItems: 'center',      // 縦方向の中央揃え
+      justifyContent: 'center',  // 横方向の中央揃え
+      backgroundColor: '#ff8c00',// 背景色を明るいオレンジに設定
+      textAlign: 'center',       // テキストを中央揃え
+    }}
+      
+    >
+      <Typography variant="h4" component="h2" sx={{ marginBottom: '1rem'}}>
+        なにもの？
+      </Typography>
+      <Typography variant="body1">
+        情報系学部の3年生です。ゲーム、Webなど作ったりしています。
+      </Typography>
+
+      <Typography variant="body1">
+        登山、
+        キャンプ、旅行もそこそこ好きです。
+      </Typography>
+    </Box>
   );
 }
 
